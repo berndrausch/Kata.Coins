1a4521e3ede290502c5ff5dcb42d8bc6471f1ccc
berndrausch <bernd@rauschpost.de>
2017-12-30 13:50:16 +0100

    solution setup

---
+++ Kata.Coins.ConosleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata.Coins.ConosleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Kata.Coins.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.Coins.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
